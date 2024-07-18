29df8dc2d4f0a1fa493d2158a303f2f3fe743c26
Space Nerd <71553327+SpaceNerde@users.noreply.github.com>
2024-07-18 15:20:55 +0200

    to the web and beyond

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-cargo build --target wasm32-unknown-unknown
+cargo build --release --target wasm32-unknown-unknown
+wasm-bindgen --no-typescript --target web --out-dir ./out/ --out-name "game" ./target/wasm32-unknown-unknown/release/game.wasm
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .run();
+    let mut app = App::new();
+    app.add_plugins(DefaultPlugins.set(WindowPlugin {
+        primary_window: Some(Window {
+            title: "Game".to_string(),
+            fit_canvas_to_parent: true,
+            prevent_default_event_handling: false,
+            ..default()
+        }),
+        ..default()
+    }));
 }
